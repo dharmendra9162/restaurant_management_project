806fd0f5916756e10260b44b9cfd96c83799da3c
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-08-07 15:42:22 +0530

    21764

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests 
+from django.shortcuts import render
+
+def menu_home (request):
+    response = requests('http://localhost:8000/api/menu/')
+    if response.status_code == 200:
+        menu_items = response.json()
+    else:
+        menu_items = []
+    return render(request,'menu.html',{'menu_items':menu_items})    
