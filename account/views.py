72810b9da5befa16219cc1c61253dc873f8b5bbe
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-08-08 10:21:45 +0530

    21767

--- account/views.py
+++ account/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import response
 
-# Create your views here.
-import requests 
-from django.shortcuts import render
-
-def menu_home (request):
-    response = requests('http://localhost:8000/api/menu/')
-    if response.status_code == 200:
-        menu_items = response.json()
-    else:
-        menu_items = []
-    return render(request,'menu.html',{'menu_items':menu_items})    
+@api_view['GET']
+def get_menu(request):
+    menu =[
+        {
+            "name":"Margherita Pizza"
+            "description":"classic Pizza with tomato sauce, and basil",
+            "Price":10.30
+        },
+        {
+            "name":"pasta Alfredo",
+            "description": "creamy Alfredo sauce with fettuccicine pasta",
+            "price":10.40
+        }
+    ]
+    return response(menu)
