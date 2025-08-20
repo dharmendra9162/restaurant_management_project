e1ce982cb5f554501b417d7c1dd6f6cf9ce010da
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-08-20 10:03:32 +0530

    34993

--- account/urls.py
+++ account/urls.py
@@ -4,3 +4,11 @@ from .views import get_menu
 urlpatterns = [
     path('api/menu/',get_menu,name='get_menu')
 ]
+
+
+from django.urls import path
+from .views import home 
+
+urlpatterns=[
+    path("", home, name="home")
+]
