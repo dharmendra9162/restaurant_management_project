255f4ae24c68f6f838f965aacb509adeb3104029
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-08-25 10:20:24 +0530

    37567

--- account/models.py
+++ account/models.py
@@ -10,3 +10,12 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.username
+
+from django.db import models
+
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
