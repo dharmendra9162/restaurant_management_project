6d99fb78c7edb7aa8e6a8282176bac92b9671681
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-02 15:43:20 +0530

    41418

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def menu_view(request):
+    items = MenuItem.objects.all()
+    return render(request,"menu.html",{"items": items})
