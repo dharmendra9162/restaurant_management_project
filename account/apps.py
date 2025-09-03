f70e89482b86ede7b4d080274edb18c21e21b40f
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-03 18:54:23 +0530

    41415

--- account/apps.py
+++ account/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
+
+
+class contactForm(forms.ModelForm):
+    class Meta:
+        model = contact
+        fields = ['name',email]
