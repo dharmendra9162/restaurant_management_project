2f2344a2833334dea08dd191ee793d575460249c
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-11 10:40:00 +0530

    43590

--- account/urls.py
+++ account/urls.py
@@ -29,3 +29,6 @@ from . import views
 urlpatterns =[
     path('',views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
