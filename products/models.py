0a2842758e6891e50b40147f2e1e5c751b3f07bb
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-11 10:29:56 +0530

    43590

--- products/models.py
+++ products/models.py
@@ -8,3 +8,15 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+
+from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    Image = models.ImageField(upload_to="menu_image/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
