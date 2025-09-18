8198a05752248d785ff821df2882a962e0de8442
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-18 10:21:41 +0530

    44329

--- account/urls.py
+++ account/urls.py
@@ -41,4 +41,15 @@ urlpatterns =[
     path("about/", view.about, name="about"),
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import view
+
+urlpatterns =[
+    path("", views.home, name="home"),
+]
+
+if settings.DEBUG
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
