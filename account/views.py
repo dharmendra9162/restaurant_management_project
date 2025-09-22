767343cdca755034c37d90fad1ba2be7f658fb36
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-22 11:32:22 +0530

    44330

--- account/views.py
+++ account/views.py
@@ -230,3 +230,11 @@ def add_to_cart(request, item_id):
 
  def about (request):
     return render(request, "about.html")
+
+
+    from django.shortcuts import render
+    from .models import Restaurant
+
+    def home(request):
+        restaurant = Restaurant.objects.first()
+        return render(request, "home.html", {"restaurant"}):restaurant
