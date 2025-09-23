db61f58bb563e67874930fd885c9ce80bab1ee5d
Dharmendra Sharma <dharmendra9162sharma@gmail.com>
2025-09-23 16:39:08 +0530

    50300

--- account/models.py
+++ account/models.py
@@ -99,4 +99,11 @@ class Restaurant(models.model):
 
     def __str__(self):
         return self.name
-        
+
+from django.db import models
+
+class MenuCategory(models.model):
+    name = models.CharField(max_length=100, unique=True)
+
+ def __str__(self):
+    return self.name       
